2202e5ba507959cffb60e03006d4fe0d4c06b9b3
admin <admin@Vernigora-PC.local>
2014-04-25 15:00:24 +0300

    Initial Commit

---
+++ AccountInfoStart/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AccountInfoStart
+//
+//  Created by admin on 25.04.14.
+//  Copyright (c) 2014 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AccountInfoStart/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AccountInfoStart
+//
+//  Created by admin on 25.04.14.
+//  Copyright (c) 2014 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
